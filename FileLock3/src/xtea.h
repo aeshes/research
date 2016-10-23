3d27fe0692c84b68028bb66acc1cd38413b2554c
aeshes <mathesis@inbox.ru>
2016-10-23 23:39:49 +0300

    Encrypting files, XTEA + RSA session key encrypting

---
+++ FileLock3/src/xtea.h
@@ -0,0 +1,32 @@
+#ifndef XTEA_H
+#define XTEA_H
+
+#include <stdint.h>
+#include "rsa.h"
+
+#define XTEA_GENERATE_LETTERS	58
+#define XTEA_KEY_SIZE			16
+#define XTEA_BLOCK_SIZE			8
+#define BLOCKS_TO_ENCRYPT		8
+
+typedef struct XTEA_DATA
+{
+	int KEY_SIZE;
+	int XTEA_KEY_RANGE;
+	uint32_t XTEA_KEY_HASH;
+	char xtea_key[16];
+	char rsa_encrypted_key[RSA_BLOCK_SIZE];
+} XTEA_DATA, *PXTEA_DATA;
+
+
+void xtea_init(XTEA_DATA *xtea_data, int key_size, int key_range);
+
+void gen_xtea_key(XTEA_DATA *xtea_data);
+
+/* take 64 bits of data in v[0] and v[1] and 128 bits of key[0] - key[3] */
+
+void xtea_encrypt(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]);
+
+void xtea_decrypt(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]);
+
+#endif
